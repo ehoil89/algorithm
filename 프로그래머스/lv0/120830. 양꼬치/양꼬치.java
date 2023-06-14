bf255dd09836afdc68ab7c59e281724581db71f1
ehoil89 <108660134+ehoil89@users.noreply.github.com>
2023-06-14 17:00:38 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int k2 = (int) n / 10;
+       
+        return answer = (12000*n) + (2000*k) - (2000*k2);
+    }
+}
